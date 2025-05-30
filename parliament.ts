e6dd81f278edf4a1f51affc88f43ba874d7744c1
DaInfLoop <github@dainfloop.is-a.dev>
2025-05-30 22:57:08 +0100

    Edits to parliament.ts
    This commit does the following to "parliament.ts":
      - Make clearer what each exported item does. This has been done by renaming Parties to PartiesEnum and PartyLeaders to Parties
      - Add the Peanut Gallery as a valid party that can vote. 2 seats have been given, one for Astra and one for Lenny
      - Add seatholder IDs to each party
      - Change the ID of the current Prime Minister from Souptik to Emma: Souptik had been present due to a mis-reading of the #parliament canvas

--- parliament.ts
+++ parliament.ts
@@ -1,5 +1,5 @@
 // This file should be kept up to date with the current assembly/prime minister.
-export enum Parties {
+export enum PartiesEnum {
     // Parties
     HCP,
     hUWUp,
@@ -12,7 +12,10 @@ export enum Parties {
 
     // Independent Candidates
     Ryan,
-    Loop
+    Loop,
+
+    // Peanut Gallery
+    Peanut,
 }
 
 export type PartyInformation = {
@@ -22,76 +25,107 @@ export type PartyInformation = {
     seats: number
 }
 
-export const PartyLeaders: Record<Parties, PartyInformation> = {
-    [Parties.HCP]: {
+export const Parties: Record<PartiesEnum, PartyInformation> = {
+    [PartiesEnum.HCP]: {
         name: "Hack Club Communist Party",
-        seatholders: [],
+        seatholders: [
+            'U0807ADEC6L', // Manan
+            'U07E4196AMA', // Souptik
+        ],
         shortName: "HCP",
         seats: 40
     },
 
-    [Parties.hUWUp]: {
+    [PartiesEnum.hUWUp]: {
         name: "Hack Club UwU Party",
-        seatholders: [],
+        seatholders: [
+            'U08EMT46G3V', // Lynn
+        ],
         shortName: "hUwUp",
         seats: 14
     },
 
-    [Parties.HGP]: {
+    [PartiesEnum.HGP]: {
         name: "Hack Club Gay Party",
-        seatholders: [],
+        seatholders: [
+            'U06JLP2R8JV', // Firepup
+        ],
         shortName: "HGP",
         seats: 9
     },
 
-    [Parties.WME]: {
+    [PartiesEnum.WME]: {
         name: "Church of Windows M.E.",
-        seatholders: [],
+        seatholders: [
+            'U07F2P4EDBP', // Lucas
+        ],
         shortName: "WME",
         seats: 7
     },
 
-    [Parties.HDP]: {
+    [PartiesEnum.HDP]: {
         name: "Hack Club Dino Party",
-        seatholders: [],
+        seatholders: [
+            'U08B60DCYG2', // Manni
+        ],
         shortName: "HDP",
         seats: 7
     },
 
-    [Parties.HCRP]: {
+    [PartiesEnum.HCRP]: {
         name: "Hack Club Republic Party",
-        seatholders: [],
+        seatholders: [
+            'U083T3ZP6AV', // Navdeep
+        ],
         shortName: "HCRP",
         seats: 12
     },
 
-    [Parties.Colon3]: {
+    [PartiesEnum.Colon3]: {
         name: ":3 Party",
-        seatholders: [],
+        seatholders: [
+            'U08JMV3PEGN', // Emma
+        ],
         shortName: ":3",
         seats: 22
     },
 
-    [Parties.HCLP]: {
+    [PartiesEnum.HCLP]: {
         name: "Hack Club Linux Party",
-        seatholders: [],
+        seatholders: [
+            'U07H41WMEEL', // Samannoy
+        ],
         shortName: "HCLP",
         seats: 10
     },
 
-    [Parties.Ryan]: {
+    [PartiesEnum.Ryan]: {
         name: "(Independent) Ryan",
-        seatholders: [],
+        seatholders: [
+            'U07NKS9S8GZ', // Ryan
+        ],
         shortName: "(Independent) Ryan",
         seats: 21
     },
 
-    [Parties.Loop]: {
+    [PartiesEnum.Loop]: {
         name: "(Independent) Loop",
-        seatholders: [],
+        seatholders: [
+            'U06TBP41C3E', // Loop
+        ],
         shortName: "(Independent) Loop",
         seats: 10
+    },
+
+    [PartiesEnum.Peanut]: {
+        name: "Peanut Gallery",
+        seatholders: [
+            'U089924LMK8', // Astra
+            'U079YUX7220', // Lenny
+        ],
+        shortName: "Peanut Gallery",
+        seats: 2
     }
 }
 
-export const PRIME_MINISTER = "U07E4196AMA"
+export const PRIME_MINISTER = "U08JMV3PEGN" // Emma
