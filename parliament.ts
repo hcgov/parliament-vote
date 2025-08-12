94af2b912ee9c73e40015ad763481090efb98942
DaInfLoop <github@dainfloop.is-a.dev>
2025-08-12 12:33:23 +0500

    there is no parliamentary assembly in ba sing se

--- parliament.ts
+++ parliament.ts
@@ -1,19 +1,6 @@
 // This file should be kept up to date with the current assembly/prime minister.
 export enum PartiesEnum {
-    // Parties
-    HCP,
-    HGP,
-    WME,
-    HDP,
-    HCRP,
-    Colon3,
-    HCLP,
 
-    // Independent Candidates
-    Ryan,
-
-    // Peanut Gallery
-    Peanut,
 }
 
 export type PartyInformation = {
@@ -24,90 +11,7 @@ export type PartyInformation = {
 }
 
 export const Parties: Record<PartiesEnum, PartyInformation> = {
-    [PartiesEnum.HCP]: {
-        name: "Hack Club Communist Party",
-        seatholders: [
-            { userId: 'U0807ADEC6L', seats: 20 }, // Manan
-            { userId: 'U07E4196AMA', seats: 19 }, // Souptik
-            { userId: 'U07NXNK171N', seats: 1 } // Taran
-        ],
-        shortName: "HCP",
-        seats: 40
-    },
 
-    [PartiesEnum.HGP]: {
-        name: "Hack Club Gay Party",
-        seatholders: [
-            { userId: 'U06JLP2R8JV', seats: 9 }, // Firepup
-        ],
-        shortName: "HGP",
-        seats: 9
-    },
-
-    [PartiesEnum.WME]: {
-        name: "Church of Windows M.E.",
-        seatholders: [
-            { userId: 'U07F2P4EDBP', seats: 7 }, // Lucas
-        ],
-        shortName: "WME",
-        seats: 7
-    },
-
-    [PartiesEnum.HDP]: {
-        name: "Hack Club Dino Party",
-        seatholders: [
-            { userId: 'U08B60DCYG2', seats: 31 }, // Manni
-        ],
-        shortName: "HDP",
-        seats: 31
-    },
-
-    [PartiesEnum.HCRP]: {
-        name: "Hack Club Republic Party",
-        seatholders: [
-            { userId: 'U083T3ZP6AV', seats: 12 }, // Navdeep
-        ],
-        shortName: "HCRP",
-        seats: 12
-    },
-
-    [PartiesEnum.Colon3]: {
-        name: ":3 Party",
-        seatholders: [
-            { userId: 'U0915TRKB6U', seats: 22 }, // Emma
-        ],
-        shortName: ":3",
-        seats: 22
-    },
-
-    [PartiesEnum.HCLP]: {
-        name: "Hack Club Linux Party",
-        seatholders: [
-            { userId: 'U07H41WMEEL', seats: 10 }, // Samannoy
-        ],
-        shortName: "HCLP",
-        seats: 10
-    },
-
-    [PartiesEnum.Ryan]: {
-        name: "(Independent) Ryan",
-        seatholders: [
-            { userId: 'U07NKS9S8GZ', seats: 21 }, // Ryan
-        ],
-        shortName: "(Independent) Ryan",
-        seats: 21
-    },
-
-    [PartiesEnum.Peanut]: {
-        name: "Peanut Gallery",
-        seatholders: [
-            { userId: 'U089924LMK8', seats: 1 }, // Astra
-            { userId: 'U079YUX7220', seats: 1 }, // Lenny
-            { userId: 'U08EMT46G3V', seats: 1 }, // Lynn
-        ],
-        shortName: "Peanut Gallery",
-        seats: 3
-    }
 }
 
-export const PRIME_MINISTER = "U0915TRKB6U" // Emma
+export const PRIME_MINISTER = "" // <NONE>
