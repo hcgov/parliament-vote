049cedb3aa3d46cb967377f4db856f3859ee8c9a
DaInfLoop <github@dainfloop.is-a.dev>
2025-08-30 20:04:30 +0100

    new parliament!!

--- parliament.ts
+++ parliament.ts
@@ -2,7 +2,19 @@ export const IS_PARLIAMENT_IN_SESSION = false;
 
 // This file should be kept up to date with the current assembly/prime minister.
 export enum PartiesEnum {
+    // Parties
+    AVCAP,
+    Colon3,
+    PAR,
+    WSP,
+    XREP,
+    WME,
 
+    // Independents
+    Lexm,
+
+    // Peanut Gallery
+    Peanut
 }
 
 export type PartyInformation = {
@@ -13,7 +25,129 @@ export type PartyInformation = {
 }
 
 export const Parties: Record<PartiesEnum, PartyInformation> = {
+    [PartiesEnum.AVCAP]: {
+        name: "Anti-Vibe Coding and PHP Party",
+        seatholders: [
+            {
+                userId: 'U08D3AY7BG8', // Gizzy
+                seats: 15
+            },
+            {
+                userId: 'U08RJ1PEM7X', // Nova
+                seats: 15
+            },
+            {
+                userId: 'U08R4SW3FU7', // Adrian
+                seats: 14
+            }
+        ],
+        shortName: "AVCAP",
+        seats: 44
+    },
 
+    [PartiesEnum.Colon3]: {
+        name: ":3 Party",
+        seatholders: [
+            {
+                userId: 'U07LECV4Q59', // Stars
+                seats: 10
+            },
+            {
+                userId: 'U0915TRKB6U', // Emma
+                seats: 14
+            },
+            {
+                userId: 'U08JN012F7G', // Hayden
+                seats: 10
+            },
+            {
+                userId: 'U093T2F9UKW', // Birdy
+                seats: 10
+            }
+        ],
+        shortName: ":3",
+        seats: 44
+    },
+
+    [PartiesEnum.PAR]: {
+        name: "Parmesan",
+        seatholders: [
+            {
+                userId: 'U081C6XT885', // Sahana
+                seats: 24
+            }
+        ],
+        shortName: "PAR",
+        seats: 24,
+    },
+
+    [PartiesEnum.XREP]: {
+        name: "X Republic",
+        seatholders: [
+            {
+                userId: 'U083T3ZP6AV', // Navdeep
+                seats: 10
+            }
+        ],
+        shortName: 'XREP',
+        seats: 10
+    },
+
+    [PartiesEnum.WME]: {
+        name: "Church of Windows ME",
+        seatholders: [
+            {
+                userId: 'U07F2P4EDBP', // Lucas
+                seats: 7
+            }
+        ],
+        shortName: 'WME',
+        seats: 10
+    },
+
+    [PartiesEnum.Lexm]: {
+        name: "(Independent) lexm",
+        seatholders: [
+            {
+                userId: 'U079UHJDBRT', // Lennier
+                seats: 24
+            }
+        ],
+        shortName: '(Independent) lexm',
+        seats: 25
+    },
+
+    [PartiesEnum.WSP]: {
+        name: "Whitestone Inc., Political Division",
+        seatholders: [
+            {
+                userId: 'U02KQ9WQT0A', // Krish
+                seats: 17
+            }
+        ],
+        seats: 17,
+        shortName: 'WSP'
+    },
+
+    [PartiesEnum.Peanut]: {
+        name: "Peanut Gallery",
+        seatholders: [
+            {
+                userId: 'U089924LMK8', // Astra
+                seats: 1
+            },
+            {
+                userId: 'U08EMT46G3V', // Lynn
+                seats: 1
+            },
+            {
+                userId: 'U079YUX7220', // Lenny
+                seats: 1
+            },            
+        ],
+        seats: 3,
+        shortName: "Peanut Gallery"
+    }
 }
 
-export const PRIME_MINISTER = "" // <NONE>
+export const PRIME_MINISTER = "U0915TRKB6U" // Emma
